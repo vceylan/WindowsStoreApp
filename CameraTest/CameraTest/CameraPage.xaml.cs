c465b6b67f89999b56e1f8dd9da284591b55857d
vceylan <veysel.ceylan@cronom.com>
2015-07-30 17:14:55 +0300

    CameraTestApp - first commit

---
+++ CameraTest/CameraTest/CameraPage.xaml.cs
@@ -0,0 +1,173 @@
+﻿using CameraTest.Common;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.Graphics.Display;
+using Windows.Media.Capture;
+using Windows.Storage;
+using Windows.Storage.Streams;
+using Windows.UI.Popups;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Media.Imaging;
+using Windows.UI.Xaml.Navigation;
+
+// The Basic Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=234237
+
+namespace CameraTest
+{
+    /// <summary>
+    /// A basic page that provides characteristics common to most applications.
+    /// </summary>
+    public sealed partial class CameraPage : Page
+    {
+        private enum DialogAnswer { Yes, No};
+        private NavigationHelper navigationHelper;
+        private ObservableDictionary defaultViewModel = new ObservableDictionary();
+
+        /// <summary>
+        /// This can be changed to a strongly typed view model.
+        /// </summary>
+        public ObservableDictionary DefaultViewModel
+        {
+            get { return this.defaultViewModel; }
+        }
+
+        /// <summary>
+        /// NavigationHelper is used on each page to aid in navigation and 
+        /// process lifetime management
+        /// </summary>
+        public NavigationHelper NavigationHelper
+        {
+            get { return this.navigationHelper; }
+        }
+
+
+        public CameraPage()
+        {
+            this.InitializeComponent();
+            this.navigationHelper = new NavigationHelper(this);
+            this.navigationHelper.LoadState += navigationHelper_LoadState;
+            this.navigationHelper.SaveState += navigationHelper_SaveState;
+        }
+
+        /// <summary>
+        /// Populates the page with content passed during navigation. Any saved state is also
+        /// provided when recreating a page from a prior session.
+        /// </summary>
+        /// <param name="sender">
+        /// The source of the event; typically <see cref="NavigationHelper"/>
+        /// </param>
+        /// <param name="e">Event data that provides both the navigation parameter passed to
+        /// <see cref="Frame.Navigate(Type, Object)"/> when this page was initially requested and
+        /// a dictionary of state preserved by this page during an earlier
+        /// session. The state will be null the first time a page is visited.</param>
+        private void navigationHelper_LoadState(object sender, LoadStateEventArgs e)
+        {
+        }
+
+        /// <summary>
+        /// Preserves state associated with this page in case the application is suspended or the
+        /// page is discarded from the navigation cache.  Values must conform to the serialization
+        /// requirements of <see cref="SuspensionManager.SessionState"/>.
+        /// </summary>
+        /// <param name="sender">The source of the event; typically <see cref="NavigationHelper"/></param>
+        /// <param name="e">Event data that provides an empty dictionary to be populated with
+        /// serializable state.</param>
+        private void navigationHelper_SaveState(object sender, SaveStateEventArgs e)
+        {
+        }
+
+        #region NavigationHelper registration
+
+        /// The methods provided in this section are simply used to allow
+        /// NavigationHelper to respond to the page's navigation methods.
+        /// 
+        /// Page specific logic should be placed in event handlers for the  
+        /// <see cref="GridCS.Common.NavigationHelper.LoadState"/>
+        /// and <see cref="GridCS.Common.NavigationHelper.SaveState"/>.
+        /// The navigation parameter is available in the LoadState method 
+        /// in addition to page state preserved during an earlier session.
+
+        protected override void OnNavigatedTo(NavigationEventArgs e)
+        {
+            navigationHelper.OnNavigatedTo(e);
+        }
+
+        protected override void OnNavigatedFrom(NavigationEventArgs e)
+        {
+            navigationHelper.OnNavigatedFrom(e);
+        }
+
+        #endregion
+
+        private async void TakePhoto_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                // Using Windows.Media.Capture.CameraCaptureUI API to capture a photo
+                var dialog = new CameraCaptureUI();
+                Size aspectRatio = new Size(16, 9);
+                dialog.PhotoSettings.CroppedAspectRatio = aspectRatio;
+                var test = DisplayProperties.ResolutionScale;
+                StorageFile file = await dialog.CaptureFileAsync(CameraCaptureUIMode.Photo);
+                if (file != null)
+                {
+                    BitmapImage bitmapImage = new BitmapImage();
+                    using (IRandomAccessStream fileStream = await file.OpenAsync(FileAccessMode.Read))
+                    {
+                        bitmapImage.SetSource(fileStream);
+                    }
+
+                    emptyInfoText.Visibility = Visibility.Collapsed;
+
+                    Image image = new Image();
+                    image.Source = bitmapImage;
+                    image.Height = image.Width = 200;
+                    image.Stretch = Stretch.UniformToFill;
+                    photoCollection.Items.Add(image);
+                }
+                else
+                {
+                    var messageDialog = new MessageDialog("No photo captured.", "Capture Error");
+                    messageDialog.ShowAsync();
+                }
+            }
+            catch (Exception ex)
+            {
+                var messageDialog = new MessageDialog(ex.Message, "Error");
+                messageDialog.ShowAsync();
+            }
+        }
+
+        private void photoCollection_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            var photoCollection = sender as GridView;
+            if (photoCollection == null) return;
+            if (photoCollection.SelectedItems.Count > 2)
+            {
+                photoCollection.SelectedItems.Remove(photoCollection.SelectedItems[0]);
+            }
+        }
+
+        private async void backButton_Click(object sender, RoutedEventArgs e)
+        {
+            var messageDialog = new MessageDialog("Do you want to quit before saving?", "Warning");
+            messageDialog.Commands.Add(new UICommand("Yes", null, DialogAnswer.Yes));
+            messageDialog.Commands.Add(new UICommand("No", null, DialogAnswer.No));
+            var command = await messageDialog.ShowAsync();
+            if ((DialogAnswer)command.Id == DialogAnswer.Yes)
+            {
+                navigationHelper.GoBack();
+            }
+        }
+    }
+}
